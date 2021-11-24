ea50384040a5d0bdb735417fad1d50e031a039ce
jchancellor-ms <jchancellor@microsoft.com>
2021-11-24 15:25:05 -0800

    updated providers block so configuration would run

--- tfstate_template/providers.tf
+++ tfstate_template/providers.tf
@@ -9,13 +9,16 @@ terraform {
     }
   }
 
-  
+  /*
   backend "azurerm" {
     resource_group_name = "<replace with tfstate resource group name>"
     storage_account_name = "<replace with tfstate storage account name>"
     container_name = "<replace with tfstate storage account container name>"
     key = "<replace with a filename for the state file with .tfstate suffix>"
   }
+  */
+
+
 }
 
 
