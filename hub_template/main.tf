d1f3fa27595875450ac006eea2c06bdb0f2fbf0a
jchancellor-ms <jchancellor@microsoft.com>
2021-11-24 18:05:08 -0800

    Corrected typos

--- hub_template/main.tf
+++ hub_template/main.tf
@@ -1,5 +1,5 @@
 module "azure_hub_with_firewall_and_bastion" {
-  source = "github.com/jchancellor-ms/terraform//modules/azure_hub_with_firewall_and_bastion?ref=v0.0.7"
+  source = "github.com/jchancellor-ms/terraform//modules/azure_hub_with_firewall_and_bastion?ref=v0.0.8"
 
   hub_vnet_name          = var.hub_vnet_name
   vnet_address_space     = var.vnet_address_space
@@ -12,7 +12,7 @@ module "azure_hub_with_firewall_and_bastion" {
   bastion_pip_name       = var.bastion_pip_name
   bastion_name           = var.bastion_name
   firewall_sku_tier      = var.firewall_sku_tier
-  firewall_pip_name      = var.firweall_pip_name
+  firewall_pip_name      = var.firewall_pip_name
   firewall_name          = var.firewall_name
   log_analytics_id       = var.log_analytics_id
 }
