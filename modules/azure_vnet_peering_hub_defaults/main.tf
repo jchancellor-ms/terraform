282c6640732fea279f1943d00c97298ef1ee7d4e
jchancellor-ms <jchancellor@microsoft.com>
2021-11-30 10:37:25 -0800

    Added Hub peer module

---
+++ modules/azure_vnet_peering_hub_defaults/main.tf
@@ -0,0 +1,11 @@
+resource "azurerm_virtual_network_peering" "hub_owned_peer" {
+  name                      = "${var.spoke_vnet_name}-link"
+  resource_group_name       = var.hub_rg_name
+  virtual_network_name      = var.hub_vnet_name
+  remote_virtual_network_id = var.spoke_vnet_id
+    
+  allow_virtual_network_access = true
+  allow_forwarded_traffic = true 
+  allow_gateway_transit = true
+  use_remote_gateways = false
+}
